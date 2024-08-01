0111059f7e53bdcb25f35631e2d19d388b3ff735
dddwsd <quswotmd5@naver.com>
2024-08-02 01:04:12 +0900

    (LeetCode) Add kSum algorithm to solution of 18th problem

--- leetcode/18.py
+++ leetcode/18.py
@@ -108,6 +108,50 @@ class Solution:
                         right -= 1
         return result
 
+"""
+kSum algorithm
+Runtime
+64ms / 95.91%
+Memory
+16.70MB / 25.62%
+"""
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+
+        def kSum(nums, start, target, k):
+            res = []
+            if start >= len(nums):
+                return res
+            if target < nums[start]*k or target > nums[-1]*k:
+                return res
+            if k == 2:
+                return twoSum(nums, start, target)
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                for subset in kSum(nums, i + 1, target - nums[i], k - 1):
+                    res.append([nums[i]] + subset)
+            return res
+
+        def twoSum(nums, start, target):
+            ans = []
+            i, j = start, len(nums) - 1
+            while i < j:
+                total = nums[i] + nums[j]
+                if total < target or (i > start and nums[i] == nums[i - 1]):
+                    i += 1
+                elif total > target or (j < len(nums) - 1 and nums[j] == nums[j + 1]):
+                    j -= 1
+                else:
+                    ans.append([nums[i], nums[j]])
+                    i += 1
+                    j -= 1
+            return ans
+
+        nums.sort()
+        return kSum(nums, 0, target, 4)
+
+
 
 solution = Solution()
 nums = [1,0,-1,0,-2,2]
